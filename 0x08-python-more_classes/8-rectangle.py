1dfc5f6ea0ac10237e5005533fdbb34578582ea5
tobestfol2 <tobestfol2@gmail.com>
2023-09-04 17:07:19 -0700

    ./8-main.py

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Module 8-rectangle
+"""
+
+
+class Rectangle:
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        Rectangle.number_of_instances += 1
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.validate_value(value, "width")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.validate_value(value, "height")
+        self.__height = value
+
+    def validate_value(self, value, attribute_name):
+        if not isinstance(value, int):
+            raise TypeError(f"{attribute_name} must be an integer")
+        if value < 0:
+            raise ValueError(f"{attribute_name} must be >= 0")
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return '\n'.join(['#' * self.__width for _ in range(self.__height)])
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect1, rect2):
+        if not isinstance(rect1, Rectangle):
+            raise TypeError("rect1 must be an instance of Rectangle")
+        if not isinstance(rect2, Rectangle):
+            raise TypeError("rect2 must be an instance of Rectangle")
+        return rect1 if rect1.area() >= rect2.area() else rect2
