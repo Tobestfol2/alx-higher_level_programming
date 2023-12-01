ac5d2379d3f657b29a749b5605ff258e11370882
tobestfol2 <tobestfol2@gmail.com>
2023-12-01 15:10:56 -0800

    1. Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,33 +1,23 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-=======
-"""This module makes a post request
-to the given email"""
->>>>>>> 84eba66b01c133761453f09680095399be9e946a
+"""
+    This module prints the X-Request-Id
+    variable in the response
+"""
+
 import urllib.request
-import urllib.parse
 import sys
 
-# Check if the correct number of arguments are provided
-if len(sys.argv) != 3:
-    print("Usage: python script.py <URL> <email>")
+if len(sys.argv) != 2:
+    print("Usage: ./1-hbtn_header.py <URL>")
     sys.exit(1)
 
 url = sys.argv[1]
-email = sys.argv[2]
-
-# Prepare the data to send in the POST request
-data = {'email': email}
-data = urllib.parse.urlencode(data).encode('utf-8')
-req = urllib.request.Request(url, data)
 
 try:
+    req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
-        res = response.read().decode('utf-8')
-    print(res)
-except urllib.error.URLError as e:
-    print("URLError:", e)
-except urllib.error.HTTPError as e:
-    print("HTTPError:", e)
+        header = response.headers.get('X-Request-Id')
+        if header is not None:
+            print(header)
 except Exception as e:
-    print("An error occurred:", e)
+    print(e)
